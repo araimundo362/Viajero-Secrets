f454615d54571455a9aefaaee535c7d5ec2781b4
Tony362 <araimundo362@gmail.com>
2021-05-21 17:01:15 -0300

    feat(): adds links to redirect

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+
 $(() => {
 
     $(window).on("scroll", () => {
@@ -51,4 +52,48 @@ $("#meetInput").on("blur", () => {
     if (meet.val().length > 0) {
         meet.addClass('successInput');
     }
+})
+
+$('#submitButton').on("click", async () => {
+    let name = $('#nameAsesoriaInput'); //string
+    let email = $('#emailAsesoriaInput'); // string
+    let meet = $('#meetAsesoriaInput'); 
+    let asesoria = $('#asesoriaSelect'); // PERSONALIZADA -- REDES
+    let date = $('#dateAsesoriaInput'); //2021-05-08 type string
+
+    if (!nameControl(name.val())) {
+        name.css("border-color", "red")
+    }
+    if (!emailControl(email.val())) {
+        email.css("border-color", "red");
+    }
+    if (!asesoriaControl(asesoria.val())) {
+        asesoria.css("border-color", "red");
+    }
+    if (!dateControl(date.val())) {
+        date.css("border-color", "red");
+    }
+
+    if (nameControl(name.val()) && emailControl(email.val()) && asesoriaControl(asesoria.val()) && date.val()) {
+        console.log('Se ejecuta esto ?//?')
+        
+        let payload =   {
+            "apikey": "f40f10cc58334e1ae829fec416fcfe9d-us7",
+            "email": {
+              "email": "araimundo362@gmail.com"
+            },
+            "send_welcome": false
+          };
+          payloadString = JSON.stringify(payload);
+          let options = {
+            method: "post",
+            contentType: "application/json", // contentType property was mistyped as ContentType - case matters
+            payload: payloadString
+          }
+
+          
+          let result = UrlFetchApp.fetch("https://us7.api.mailchimp.com/2.0/lists/subscribe.json", options);
+          console.log('result', result);
+    }
+    // Consultar opcion para enviar mail
 })
